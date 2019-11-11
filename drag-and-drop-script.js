3096d18929d09d81f1b4b23e91a9b637f272901a
Gino Villalpando <grieveous@live.com>
2019-11-11 12:55:22 -0700

    added drag and drop on styled div that contains random images from unsplash

--- drag-and-drop-script.js
+++ drag-and-drop-script.js
@@ -1,16 +1,33 @@
-const fill = document.querySelector(".fill");
-const empties = document.querySelectorAll(".empty");
+let dragged;
 
-// fill listeners
-fill.addEventListener("dragstart", dragStart);
-fill.addEventListener("dragend", dragEnd);
+document.addEventListener("drag", function(event) {
 
-// drag functions
+},false);
 
-function dragStart() {
-	console.log("start");
-}
+document.addEventListener("dragstart", function(event) {
+	dragged = event.target;
+	event.target.style.opacity = .5;
+}, false);
 
-function dragEnd() {
+document.addEventListener("dragend", function(event) {
+	event.target.style.opacity = " ";
+}, false);
 
-}
+document.addEventListener("dragover", function(event) {
+	event.preventDefault();
+}, false);
+
+document.addEventListener("dragenter", function(event) {
+	if (event.target.className === "empty") {
+		event.target.style.background = "light";
+	}
+}, false);
+
+document.addEventListener("drop", function(event) {
+	event.preventDefault();
+	if (event.target.className === "empty") {
+		event.target.style.background = " ";
+		dragged.parentNode.removeChild( dragged );
+		event.target.appendChild( dragged );
+	}
+}, false);
