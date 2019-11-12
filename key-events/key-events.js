5cfe349f04721a4ea2ae477f087c6c1d44283fe9
Gino Villalpando <gvillalpando@cnm.edu>
2019-11-11 20:17:45 -0700

    added notes to html and js

--- key-events/key-events.js
+++ key-events/key-events.js
@@ -1,5 +1,7 @@
+// load function at window load
 window.addEventListener("keydown", keyColorChanger, false);
 
+// function that checks for enter key being pressed to change text color
 function keyColorChanger(key) {
 	if (key.keyCode == "13"){
 		document.getElementById("paragraphOne").style.color = "yellow";
