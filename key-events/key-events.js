507e955c1143aa89b5b233e887f4cd6578d2affe
Gino Villalpando <gvillalpando@cnm.edu>
2019-11-11 20:14:01 -0700

    added function to all three paragraphs

--- key-events/key-events.js
+++ key-events/key-events.js
@@ -2,6 +2,8 @@ window.addEventListener("keydown", keyColorChanger, false);
 
 function keyColorChanger(key) {
 	if (key.keyCode == "13"){
-		document.getElementById("paragraphOne").style.color = "blue";
+		document.getElementById("paragraphOne").style.color = "yellow";
+		document.getElementById("paragraphTwo").style.color = "red";
+		document.getElementById("paragraphThree").style.color = "green";
 	}
 }
